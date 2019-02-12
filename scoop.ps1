3089cf8d20b2e631b9cbf33d7c1648e807bfa0b6
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2019-02-12 12:22:51 +0900

    Update scoop.ps1

--- scoop.ps1
+++ scoop.ps1
@@ -1,16 +1,16 @@
 scoop install `
 7zip `
 cacert `
-curl ` 
-dig ` 
-git ` 
-jq ` 
-less ` 
-ngrok ` 
-phantomjs ` 
-postgresql ` 
-rclone ` 
-sed ` 
-time ` 
-unar ` 
+curl `
+dig `
+git `
+jq `
+less `
+ngrok `
+phantomjs `
+postgresql `
+rclone `
+sed `
+time `
+unar `
 wget
